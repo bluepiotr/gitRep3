7eac0e78724971b58115bd8efe9d511679d4ce5a
Raul <bluepit@yahoo.com>
2012-11-03 09:22:48 +0000

    HelloWorld firstCommit Sat03Nov12 09.22hrs

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+package helloWorld;
+/**
+ * 
+ */
+
+/**
+ * @author Raul
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test from Raul Gomez, uploading to GitHub");
+
+	}
+
+}
