4a50be543359f55cf11b87823e9fcfec907b19d2
Raul <bluepit@yahoo.com>
2012-11-03 11:13:27 +0000

    11:13 hrs - 03Nov12

---
+++ HelloWorld/src/helloWorld/HelloBitBucket.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class HelloBitBucket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Uploading to Bitbucket.. v1");
+
+	}
+
+}
